6173b8f8e8588444e8867c6c03cf5b4b22f39ff4
Yul310 <hsy3102@gmail.com>
2022-07-25 09:44:33 -0700

    infrastructure update 1

--- server.js
+++ server.js
@@ -2,6 +2,13 @@ const express = require('express');
 const path = require('path');
 const favicon = require('serve-favicon');
 const logger = require('morgan');
+
+// Always require and configure near the top 
+require('dotenv').config();
+
+// Connect to the database
+require('./config/database');
+
 const rowdy = require('rowdy-logger')
 
 const app = express();
