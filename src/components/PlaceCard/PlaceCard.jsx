b71ba688aa7c6b59ef8c7f102f8c6a953a871907
Yul310 <hsy3102@gmail.com>
2022-07-27 22:05:04 -0700

    placeCard updated

--- src/components/PlaceCard/PlaceCard.jsx
+++ src/components/PlaceCard/PlaceCard.jsx
@@ -28,6 +28,27 @@ export default function PlaceCard({ allPlaces, setAllPlaces,distance,duration,se
         await placeAPI.deletePlace(evt.target.value);
     }
 
+    // async function editPlace(evt) {
+    //     console.log(evt.target.value);
+    //     console.log(allCats)
+    //     // FrontEnd updating
+      
+    //     const cats = allCats.filter((cat) => cat._id === evt.target.value);
+        
+    //     cats[0].title = formData.title
+    //     console.log(cats[0])
+    //    console.log(cats[0].title )
+    //     //Backend updating
+    //     catAPI.editCat(evt.target.value, formData);
+    //     setEdit(!edit)
+    //     setUpdated(!magic)
+        
+    //     setFormData({
+    //         title: ""
+    //     })
+        
+    // }
+
 
     // function clearRoute() {
     //     setDirectionsResponse(null)
@@ -46,30 +67,33 @@ export default function PlaceCard({ allPlaces, setAllPlaces,distance,duration,se
 
                     <div
                         key={idx}
-                        className=" border-black border-[1px] rounded-md pt-2 pb-4 px-4 font-light my-3 w-[20rem] text-left"
+                        className=" border-black border-[1px] rounded-md pt-2 pb-4 px-4 font-light my-3 w-[20rem] text-left text-sm"
                         id="hardshadow"
                     >
-                        <h3 className="font-bold">{place.name.toUpperCase()}</h3>
-                      
+                       <h3 className="font-bold">{place.name.split(',')[0].toUpperCase()}</h3>
+                        <h3 className="font-medium">{place.name.split(',').shift()}</h3>
+                       
 
 
 
-                        <h3 className="font-semibold">Note: {place.note}</h3>
+                        <h3 className="font-semibold">Todo: {place.note}</h3>
 
                         <button onClick={deletePlace} value={place._id}> Delete </button>
 
                     </div>
-
-                    <div className=" flex w-80 h-8 flex-row  justify-evenly ">
+                    
+                  { distance[idx]?
+                    <div className=" flex-column w-80 h-30 ">
                     <h1>||</h1>
                     <p>&nbsp;</p>
                    
-                   <h3 className="font-bold">Distance:{distance[idx]} / </h3>
-                    <h3 className="font-bold">Duration: {duration[idx]}</h3>
+                   <h3 className="font-bold">{distance[idx]}  </h3>
+                    <h3 className="font-bold"> {duration[idx]}</h3>
                 
                     <p>&nbsp;</p>
                     <h1>||</h1>
                     </div>
+                    :null}
                 </>
             ))}
 
